073275f9640252752e9c84619d4cbe4e32ac0677
tumen <bazarovta@gmail.com>
2022-12-01 19:17:41 +0300

    new Player

--- Player.py
+++ Player.py
@@ -9,6 +9,9 @@ sword['down'] = pygame.image.load("sword/down.png") #24x44
 sword['up'] = pygame.image.load("sword/up.png") #24x44
 sword['left'] = pygame.image.load("sword/left.png") #44x24
 sword['right'] = pygame.image.load("sword/right.png") #44x24
+movement = {'right': [1, 0], 'left': [-1, 0],
+            'down': [0, 1], 'up': [0, -1]}
+
 class Player:
     '''
     Class Player
@@ -25,29 +28,34 @@ class Player:
         
     '''
     
-    def __init__(self, screen):
+    def __init__(self, screen, x=450, y=450):
         self.screen = screen
-        self.x = 300
-        self.y = 300
+        self.x = x
+        self.y = y
         self.stamina = 100.
         self.health = 100.
         self.orientation = 'down' 
         self.attack = False 
         self.weapon = 'sword'
         self.surface = image['down']
-    def move(self, event):
+        self.v = 50
+        self.cond = False
+
+    def take_orientation(self, event):
         if event.key == pygame.K_d:
             self.orientation = 'right'
-            self.x += 10
         elif event.key == pygame.K_a:
             self.orientation = 'left'
-            self.x -= 10
         elif event.key == pygame.K_s:
             self.orientation = 'down'
-            self.y += 10
         elif event.key == pygame.K_w:
             self.orientation = 'up'
-            self.y -= 10
+
+    def move(self):
+        dt = 0.1
+        if self.cond == True:
+            self.x += self.v * movement[self.orientation][0] * dt
+            self.y += self.v * movement[self.orientation][1] * dt
 
     def vector_of_attack(self):
         start = (self.x, self.y)
@@ -81,8 +89,16 @@ class Player:
                 self.screen.blit(im_w, (self.x - 11, self.y + 32))
             elif self.orientation == 'up':
                 self.screen.blit(im_w, (self.x - 11, self.y - 32 - 44))    
- 
-pygame.init()
+    
+    def get_pos(self):
+        return (self.x - 32, self.y - 32, self.x + 32, self.y + 32)
+
+    def move_on(self):
+        self.cond = True
+    
+    def move_off(self):
+        self.cond = False
+'''pygame.init()
 X = 600
 Y = 600
 screen = pygame.display.set_mode((X, Y))
@@ -91,16 +107,20 @@ player = Player(screen)
 while (status):
     screen.fill('WHITE')
     player.draw()
+    player.move()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             status = False
         elif event.type == pygame.KEYDOWN:
-            player.move(event)
+            player.move_on()
+            player.take_orientation(event)
+        elif event.type == pygame.KEYUP:
+            player.move_off()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             player.attack = True
         elif event.type == pygame.MOUSEBUTTONUP:
             player.attack = False
     
     pygame.display.update()
- 
+ '''
 
