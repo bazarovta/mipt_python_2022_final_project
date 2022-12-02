5a6eea21047489bd07593d7c41b5ea9a9551e186
bazarovta <bazarovta@gmail.com>
2022-12-03 01:55:30 +0300

    start create quest_3

---
+++ Quest_hide/enemy.py
@@ -0,0 +1,76 @@
+import math
+import pygame
+
+class Agent:
+    
+    def __init__(self,screen, x, y, v, boundary):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.v = v
+        self.boundary = boundary
+        self.r = 70
+        self.angle = math.pi/3
+        self.direction = math.pi/3
+        self.lines = 200
+        self.w = math.pi / 10
+        self.a = 10
+
+    def draw(self):
+        for i in range (self.lines // 2, 2, -1):
+            x1 = self.x + self.r * math.cos(self.direction + self.angle / i)
+            x2 = self.x + self.r * math.cos(self.direction - self.angle / i)
+            y1 = self.y + self.r * math.sin(self.direction + self.angle / i)
+            y2 = self.y + self.r * math.sin(self.direction - self.angle / i)
+            pygame.draw.line(
+                self.screen,
+                (0, 255, 0),
+                [self.x, self.y],
+                [x1, y1]
+                )
+            pygame.draw.line(
+                self.screen,
+                (0, 255, 0),
+                [self.x, self.y],
+                [x2, y2]
+                )
+        pygame.draw.circle(
+                self.screen,
+                (0, 0, 0),
+                (self.x, self.y),
+                self.a
+                )
+    def move(self):
+        dt = 0.1
+        if self.boundary[0] == 'y':
+            if self.y < self.boundary[1] and self.v < 0:
+                self.v = -self.v
+            elif self.y > self.boundary[2] and self.v > 0:
+                self.v = -self.v
+            self.y += self.v * dt
+        else:
+            if self.x < self.boundary[1] and self.v < 0:
+                 self.v = -self.v
+            elif self.x > self.boundary[2] and self.v > 0:
+                self.v = -self.v
+            self.x += self.v * dt
+        self.direction += self.w * dt
+
+
+'''WIDTH = 900
+HEIGHT = 900
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+ag = Agent(screen, 450, 450, 10, ('y', 420, 480))
+pygame.display.update()
+clock = pygame.time.Clock()
+FPS = 10
+finished = True
+while finished:
+    clock.tick(FPS)
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = False
+    ag.draw()
+    ag.move()
+    pygame.display.update()'''
