e2949c817975f283f14c7e63674ddc2dff16c8fd
bazarovta <bazarovta@gmail.com>
2022-12-12 20:23:38 +0300

    some fix

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@ class Agent:
         self.r = 70
         self.angle = math.pi/3
         self.direction = math.pi/3
-        self.lines = 500
+        self.lines = 100
         self.w = math.pi / 10
         self.a = 10
 
@@ -24,19 +24,19 @@ class Agent:
             y2 = self.y + self.r * math.sin(self.direction - self.angle / i)
             pygame.draw.line(
                 self.screen,
-                (255, 255, 0),
+                (153, 0, 0),
                 [self.x, self.y],
                 [x1, y1]
                 )
             pygame.draw.line(
                 self.screen,
-                (0, 255, 0),
+                (153, 0, 0),
                 [self.x, self.y],
                 [x2, y2]
                 )
         pygame.draw.circle(
                 self.screen,
-                (0, 0, 0),
+                (255, 100, 100),
                 (self.x, self.y),
                 self.a
                 )
@@ -54,5 +54,7 @@ class Agent:
             elif self.x > self.boundary[2] and self.v > 0:
                 self.v = -self.v
             self.x += self.v * dt
+        if self.direction >= 2*math.pi:
+            self.direction -= 2*math.pi
         self.direction += self.w * dt
 
