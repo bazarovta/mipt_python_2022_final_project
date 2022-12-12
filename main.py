d241583444555a66e9822b5afb02e9b7fd2ed6be
RusApostol <krasin0469@gmail.com>
2022-12-12 22:55:06 +0300

    Added fight to quest

--- main.py
+++ main.py
@@ -8,123 +8,124 @@ import random
 import pygame
 
 
-pygame.init()
+def main():
+    pygame.init()
 
-WIDTH = 800
-HEIGHT = 600
-
-
-FPS = 30
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-text = pygame.font.Font(None, 50)
-text_data = pygame.font.Font(None, 30)
-
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
-
-shells = []
+    WIDTH = 800
+    HEIGHT = 600
 
 
-status = True
+    FPS = 30
 
-#N = random.randint(1, 4)
-N = 3
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    text = pygame.font.Font(None, 50)
+    text_data = pygame.font.Font(None, 30)
 
-enemies = []
-
-for i in range(N):
-    enemies.append(Enemy(screen))
-
-
-player = Player(screen)
-
-
-while (status):
-    del_shells = []
-    del_enemies = []
-    screen.fill('WHITE')
-    for shell in shells:
-        shell.draw()
-    player.draw()
-    for enemy in enemies:
-        enemy.draw()
-    text_health_player = text_data.render(str(player.health), True, (0, 255, 0))
-    screen.blit(text_health_player, (20, 30))
-    for i in range(len(enemies)):
-        enemy = enemies[i]
-        text_health_enemy = text_data.render(str(enemy.health), True, (255, 0, 0))
-        screen.blit(text_health_enemy, (30 + 60 * (i + 1), 30))
     pygame.display.update()
-    clock.tick(FPS)
-        
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            status = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            player.attack = True
-            for enemy in enemies:
-                player.attack_on_enemy(enemy)
-        elif event.type == pygame.MOUSEBUTTONUP:
-            player.attack = False
-    pressed_keys = pygame.key.get_pressed()
-    player.move(pressed_keys)
-        
-    for i in range(len(enemies)):
-        enemy = enemies[i]
-        if (player.x - enemy.x - enemy.size/2) ** 2 + (player.y - enemy.y - enemy.size/2) ** 2 <= enemy.R ** 2:
-            enemy.move_near_player(player)
-            if enemy.stamina == 100:
-                shells = enemy.fire(shells, player)
-                enemy.stamina -= 1
-            elif enemy.stamina < 100 and enemy.stamina > 0:
-                enemy.stamina -= 1
-            elif enemy.stamina <= 0:
-                enemy.stamina = 100
-        else:
-            enemy.move_far_from_player()
+    clock = pygame.time.Clock()
+    finished = False
+
+    shells = []
+
+
+    status = True
+
+    #N = random.randint(1, 4)
+    N = 3
+
+    enemies = []
+
+    for i in range(N):
+        enemies.append(Enemy(screen))
+
+
+    player = Player(screen)
+
+
+    while (status):
+        del_shells = []
+        del_enemies = []
+        screen.fill('WHITE')
+        for shell in shells:
+            shell.draw()
+        player.draw()
+        for enemy in enemies:
+            enemy.draw()
+        text_health_player = text_data.render(str(player.health), True, (0, 255, 0))
+        screen.blit(text_health_player, (20, 30))
+        for i in range(len(enemies)):
+            enemy = enemies[i]
+            text_health_enemy = text_data.render(str(enemy.health), True, (255, 0, 0))
+            screen.blit(text_health_enemy, (30 + 60 * (i + 1), 30))
+        pygame.display.update()
+        clock.tick(FPS)
             
-        if enemy.health <= 0:
-            del_enemies.append(i) 
-    for i in range(len(del_enemies)):
-        enemies.pop(del_enemies[i])
-        for j in range(len(del_enemies)):
-            if del_enemies[j] > del_enemies[i]:
-                del_enemies[j] -= 1
-        
-    for i in range(len(shells)):
-        sh = shells[i]
-        sh.move()
-        if sh.hittest(player) and sh.live > 0:
-            player.health -= 0
-            sh.live = 0
-        if player.health <= 0:
-            screen.fill('RED')
-            text_score_2 = text.render('GAME OVER', True, (0, 0, 0))
-            screen.blit(text_score_2, (WIDTH / 2 - 105, HEIGHT / 2 - 50))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                status = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                player.attack = True
+                for enemy in enemies:
+                    player.attack_on_enemy(enemy)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                player.attack = False
+        pressed_keys = pygame.key.get_pressed()
+        player.move(pressed_keys)
+            
+        for i in range(len(enemies)):
+            enemy = enemies[i]
+            if (player.x - enemy.x - enemy.size/2) ** 2 + (player.y - enemy.y - enemy.size/2) ** 2 <= enemy.R ** 2:
+                enemy.move_near_player(player)
+                if enemy.stamina == 100:
+                    shells = enemy.fire(shells, player)
+                    enemy.stamina -= 1
+                elif enemy.stamina < 100 and enemy.stamina > 0:
+                    enemy.stamina -= 1
+                elif enemy.stamina <= 0:
+                    enemy.stamina = 100
+            else:
+                enemy.move_far_from_player()
+                
+            if enemy.health <= 0:
+                del_enemies.append(i) 
+        for i in range(len(del_enemies)):
+            enemies.pop(del_enemies[i])
+            for j in range(len(del_enemies)):
+                if del_enemies[j] > del_enemies[i]:
+                    del_enemies[j] -= 1
+            
+        for i in range(len(shells)):
+            sh = shells[i]
+            sh.move()
+            if sh.hittest(player) and sh.live > 0:
+                player.health -= 10
+                sh.live = 0
+            if player.health <= 0:
+                screen.fill('RED')
+                text_score_2 = text.render('GAME OVER', True, (0, 0, 0))
+                screen.blit(text_score_2, (WIDTH / 2 - 105, HEIGHT / 2 - 50))
+                pygame.display.update()
+                while status:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            status = False
+            if sh.live <= 0:
+                del_shells.append(i)
+        for i in range(len(del_shells)):
+            shells.pop(del_shells[i])
+            for j in range(len(del_shells)):
+                if del_shells[j] > del_shells[i]:
+                    del_shells[j] -= 1   
+                
+        if player.health > 0 and len(enemies) <= 0:
+            screen.fill('GREEN')
+            text_score_2 = text.render('YOU WIN', True, (0, 0, 0))
+            screen.blit(text_score_2, (WIDTH / 2 - 100, HEIGHT / 2 - 50))
             pygame.display.update()
             while status:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         status = False
-        if sh.live <= 0:
-            del_shells.append(i)
-    for i in range(len(del_shells)):
-        shells.pop(del_shells[i])
-        for j in range(len(del_shells)):
-            if del_shells[j] > del_shells[i]:
-                del_shells[j] -= 1   
-             
-    if player.health > 0 and len(enemies) <= 0:
-        screen.fill('GREEN')
-        text_score_2 = text.render('YOU WIN', True, (0, 0, 0))
-        screen.blit(text_score_2, (WIDTH / 2 - 100, HEIGHT / 2 - 50))
-        pygame.display.update()
-        while status:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    status = False
 
-pygame.quit()
+    pygame.quit()
     
--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import quest_hide as quest_3
 import quest_speed as quest_2
-#import main as quest_1
+import main as quest_1
 import pygame
 
 pygame.init()
@@ -35,7 +35,7 @@ while finished:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.pos[1] > 200 and event.pos[1] < 350:
                 if event.pos[0] > 200 and event.pos[0] < 400:
-                    #quest_1.main()
+                    quest_1.main()
                     continue
                 elif event.pos[0] > 500 and event.pos[0] < 700:
                     res = quest_2.main()
